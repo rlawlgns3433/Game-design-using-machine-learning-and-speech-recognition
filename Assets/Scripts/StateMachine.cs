d4558b922bc46049e05d1af9ddda6fdd1b2d5f75
yujin <yujin2625@gmail.com>
2023-03-08 16:01:59 +0900

    playerstate

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateMachine : MonoBehaviour
+{
+    private State currentState;
+
+    public void SwitchState(State state)
+    {
+        currentState?.Exit();
+        currentState = state;
+        currentState.Enter();
+    }
+    private void Update()
+    {
+        currentState?.Tick();
+    }
+
+}
