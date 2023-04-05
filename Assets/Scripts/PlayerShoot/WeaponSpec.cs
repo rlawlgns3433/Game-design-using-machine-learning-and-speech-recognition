1fb52c791d5fdfdfd54c618c92402517694d6cb9
yujin <yujin2625@gmail.com>
2023-04-05 15:10:09 +0900

    script 생성

---
+++ Assets/Scripts/PlayerShoot/WeaponSpec.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSpec
+{
+    //속도, 데미지, 남은 총알
+    public float bulletSpeed, damageF, bulletLeft;
+    
+
+
+
+
+
+
+
+}
