8b77d6b1b9a172c890467193dbf4f90a9c779cde
지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2023-05-03 16:26:00 +0900

    Comment

--- Assets/Scripts/Chat/CommentManager.cs
+++ Assets/Scripts/Chat/CommentManager.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections;
+using StackExchange.Redis;
+using System.Linq;
+using System.Collections.Generic;
+using System;
 
 public class CommentManager : MonoBehaviour
 {
+
+    private string m_channel = "test";
     string recentId = "";
     string chatLog = "";
+    List<string> MsgList;
     TMP_Text textUI;
     [SerializeField] GameObject commentPrefab;
     [SerializeField] ScrollRect scrollRect;
@@ -14,6 +21,54 @@ public class CommentManager : MonoBehaviour
     [SerializeField] GameObject canv;
     [SerializeField] InputField chatInput;
     GameObject previousComment;
+
+
+    #region MonoBehaviour
+
+    private void Awake()
+    {
+        StartCoroutine("SubChannel");
+    }
+
+    private void Update()
+    {
+        try
+        {
+            if (MsgList.Count > 0)
+            {
+                string[] messages = MsgList[0].Split(",", 2);
+                MsgList.RemoveAt(0);
+                AddComment(messages[0], messages[1]);
+            }
+        }
+        catch (Exception e) { }
+        
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            // Äµ¹ö½º ²¨Á® ÀÖÀ» ¶§
+            if (!canv.active)
+            {
+                canv.active = true;
+                chatInput.Select();
+            }
+            // Äµ¹ö½º ÄÑÁ® ÀÖÀ» ¶§
+            else if (canv.active)
+            {
+                chatInput.Select();
+                if (chatInput.text == "") canv.SetActive(!canv.active);
+                else
+                {
+                    Redis.Instance.Publish(m_channel, chatInput.text);
+                    AddComment("ÁöÈÆ", chatInput.text);
+                    chatInput.text = "";
+                    StartCoroutine("Waitmillisec");
+                }
+            }
+        }
+    }
+
+    #endregion
 
     public void AddComment(string id, string comment)
     {
@@ -31,34 +86,30 @@ public class CommentManager : MonoBehaviour
         previousComment = prefab;
     }
 
-    private void Update()
+    public void SubAction(RedisChannel ch, RedisValue va)
     {
-        if(Input.GetKeyDown(KeyCode.Return))
-        {
-            // Äµ¹ö½º ²¨Á® ÀÖÀ» ¶§
-            if (!canv.active)
-            {
-                canv.active = true;
-                chatInput.Select();
-            }
-            // Äµ¹ö½º ÄÑÁ® ÀÖÀ» ¶§
-            else if (canv.active)
-            {
-                chatInput.Select();
-                if (chatInput.text == "") canv.SetActive(!canv.active);
-                else
-                {
-                    Redis.Instance.Publish("test", chatInput.text);
-                    AddComment("jh1", chatInput.text);
-                    chatInput.text = "";
-                    StartCoroutine("Waitmillisec");
-                }
-            }
-        }
+        MsgList.Add(va);
     }
+
+
     IEnumerator Waitmillisec()
     {
         chatInput.Select();
+        yield return new WaitForSeconds(0.001f);
+    }
+
+    IEnumerator SubChannel()
+    {
+        try
+        {
+            Redis.Instance.Subscribe("test", SubAction);
+
+        }
+        catch (Exception e)
+        {
+
+        }
+
         yield return new WaitForSeconds(0.001f);
     }
 }
